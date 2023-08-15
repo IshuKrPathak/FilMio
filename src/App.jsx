09901c33adbbf3a1495ea7f9d1e2a988b9368ac4
Ishu Pathak <77011944+Ishukumarpathak@users.noreply.github.com>
2023-08-15 11:34:26 +0530

    redux intergation and basic structuring done

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,31 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import { fetchdatafromapi } from "./utils/api";
 
+import { useSelector, useDispatch } from 'react-redux'
+
+import { getapiconfiguration } from "./store/homeSlice";
+
 function App() {
+  const dispatch = useDispatch();
+  const {url} = useSelector((state)=>
+    state.home
+  );
+  console.log(url)
   useEffect(() => {
     apitesting();
   }, []);
   const apitesting = () => {
     fetchdatafromapi("/movie/popular").then((res) => {
       console.log(res);
+      dispatch(getapiconfiguration(res));
     });
   };
 
   return (
     <>
-      <div className="app">app</div>
+      <div className="app">
+        {url?.total_pages}
+      </div>
     </>
   );
 }
