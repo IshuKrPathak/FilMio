97bb008d7102d511b7f9aa861e8257a7835d1751
Ishu Pathak <77011944+Ishukumarpathak@users.noreply.github.com>
2023-08-16 09:57:22 +0530

    final file  structuring is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import { useState, useEffect } from "react";
-
+import {BrowserRouter,Routes,Route} from "react-router-dom"
 import "./App.css";
 import { fetchdatafromapi } from "./utils/api";
 
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from "react-redux";
 
 import { getapiconfiguration } from "./store/homeSlice";
 
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/Home/Home";
+import Details from "./pages/details/Details";
+import searchResult from "./pages/searchResult/searchResult";
+import Explore from "./pages/explore/Explore";
+import Pagenotfound from "./pages/404/Pagenotfound";
+
 function App() {
   const dispatch = useDispatch();
-  const {url} = useSelector((state)=>
-    state.home
-  );
-  console.log(url)
+  const { url } = useSelector((state) => state.home);
+  console.log(url);
   useEffect(() => {
     apitesting();
   }, []);
@@ -24,11 +30,15 @@ function App() {
   };
 
   return (
-    <>
-      <div className="app">
-        {url?.total_pages}
-      </div>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route>
+        
+      </Route>
+    </Routes>
+    
+    </BrowserRouter>
+   
   );
 }
 
---
+++ src/pages/404/Pagenotfound.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Pagenotfound.scss"
+
+const Pagenotfound = () => {
+  return (
+    <div>Pagenotfound</div>
+  )
+}
+
+export default Pagenotfound
