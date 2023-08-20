8d977a38aa752469154cc347ad917d6ef3f8122a
Ishu Pathak <pishu1766@gmail.com>
2023-08-20 15:50:03 +0530

    some modifications on hero banner and app.jsx and also logo is changed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,7 @@ import { useNavigate, useLocation } from "react-router-dom";
 
 import "./Header.scss";
 
-import ContentWrapper from "../contentWrapper/contentWrapper";
+import ContentWrapper from "../contentWrapper/ContentWrapper";
 import logo from "../../assets/FilMio-logo.png";
 
 const Header = () => {
@@ -17,46 +17,110 @@ const Header = () => {
     const [showSearch, setShowSearch] = useState("");
     const navigate = useNavigate();
     const location = useLocation();
-      const openSearch = () => {
-        setMobileMenu(false)
-        setShowSearch(true)
 
-      }
-      const openMobileMenu = () =>{
-        setMobileMenu(true)
-        setShowSearch(false)
-      }
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [location]);
+
+    const controlNavbar = () => {
+        if (window.scrollY > 200) {
+            if (window.scrollY > lastScrollY && !mobileMenu) {
+                setShow("hide");
+            } else {
+                setShow("show");
+            }
+        } else {
+            setShow("top");
+        }
+        setLastScrollY(window.scrollY);
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", controlNavbar);
+        return () => {
+            window.removeEventListener("scroll", controlNavbar);
+        };
+    }, [lastScrollY]);
+
+    const searchQueryHandler = (event) => {
+        if (event.key === "Enter" && query.length > 0) {
+            navigate(`/search/${query}`);
+            setTimeout(() => {
+                setShowSearch(false);
+            }, 1000);
+        }
+    };
+
+    const openSearch = () => {
+        setMobileMenu(false);
+        setShowSearch(true);
+    };
+
+    const openMobileMenu = () => {
+        setMobileMenu(true);
+        setShowSearch(false);
+    };
+
+    const navigationHandler = (type) => {
+        if (type === "movie") {
+            navigate("/explore/movie");
+        } else {
+            navigate("/explore/tv");
+        }
+        setMobileMenu(false);
+    };
 
     return (
-     <header className={`header ${mobileMenu ? "mobileView":""}${show}`}>
-        <ContentWrapper>
-          <div className="logo">
-            <img src={logo} alt="" />
-          </div>
-          <ul className="menu-items">
-            <l1 className="menu-item">Movies</l1>
-            <l1 className="menu-item">Tv shows </l1>
-            <l1 className="menu-item">
-              <HiOutlineSearch/>
-            </l1>
-          </ul>
-          <div className="mobile-menu-items">
-            <HiOutlineSearch/>
-            {mobileMenu ? (
-              <VscChromeClose onClick={()=>setMobileMenu(false)}/>
-              // when is is on mobile view
+        <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
+            <ContentWrapper>
+                <div className="logo" onClick={() => navigate("/")}>
+                    <img src={logo} alt="" />
+                </div>
+                <ul className="menuItems">
+                    <li
+                        className="menuItem"
+                        onClick={() => navigationHandler("movie")}
+                    >
+                        Movies
+                    </li>
+                    <li
+                        className="menuItem"
+                        onClick={() => navigationHandler("tv")}
+                    >
+                        TV Shows
+                    </li>
+                    <li className="menuItem">
+                        <HiOutlineSearch onClick={openSearch} />
+                    </li>
+                </ul>
 
-            ) : (
-              <SlMenu onClick={openMobileMenu}/>
-              // when it is on desktop menu(hamburger menu)
+                <div className="mobileMenuItems">
+                    <HiOutlineSearch onClick={openSearch} />
+                    {mobileMenu ? (
+                        <VscChromeClose onClick={() => setMobileMenu(false)} />
+                    ) : (
+                        <SlMenu onClick={openMobileMenu} />
+                    )}
+                </div>
+            </ContentWrapper>
+            {showSearch && (
+                <div className="searchBar">
+                    <ContentWrapper>
+                        <div className="searchInput">
+                            <input
+                                type="text"
+                                placeholder="Search for a movie or tv show...."
+                                onChange={(e) => setQuery(e.target.value)}
+                                onKeyUp={searchQueryHandler}
+                            />
+                            <VscChromeClose
+                                onClick={() => setShowSearch(false)}
+                            />
+                        </div>
+                    </ContentWrapper>
+                </div>
             )}
-           
-            
-
-          </div>
-        </ContentWrapper>
         </header>
-      
     );
 };
 
--- src/pages/Home/heroBanner/HeroBanner.jsx
+++ src/pages/Home/heroBanner/HeroBanner.jsx
@@ -1,61 +1,62 @@
 import React, { useState, useEffect } from "react";
-import "./HeroBanner.scss";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import "./HeroBanner.scss";
+
 import useFetch from "../../../hooks/useFetch";
-import Img from "../../../components/lazyLoadimage/Img";
-import ContentWrapper from "../../../components/contentWrapper/contentWrapper";
+
+import Img from "../../../components/lazyLoadImage/Img";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 
 const HeroBanner = () => {
-  const [background, setBackground] = useState("");
-  const [query, setQuery] = useState("");
-  const navigate = useNavigate();
-  const { url } = useSelector((state) => state.home);
+    const [background, setBackground] = useState("");
+    const [query, setQuery] = useState("");
+    const navigate = useNavigate();
+    const { url } = useSelector((state) => state.home);
+    const { data, loading } = useFetch("/movie/upcoming");
 
-  const { data, loading } = useFetch("/movie/upcoming");
-  useEffect(() => {
-    const bg =
-      url.backdrop +
-      data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
-    setBackground(bg);
-  }, [data]);
+    useEffect(() => {
+        const bg =
+            url.backdrop +
+            data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+        setBackground(bg);
+    }, [data]);
 
-  const searchqueryhandler = (event) => {
-    if (event.key === "Enter" && query.length > 0) {
-      navigate(`/search/${query}`);
-    }
-  };
+    const searchQueryHandler = (event) => {
+        if (event.key === "Enter" && query.length > 0) {
+            navigate(`/search/${query}`);
+        }
+    };
 
-  return (
-    <div className="hero-banner">
-      {!loading && (
-        <div className="backdrop-img">
-          <Img src={background} />
+    return (
+        <div className="heroBanner">
+            {!loading && (
+                <div className="backdrop-img">
+                    <Img src={background} />
+                </div>
+            )}
+
+            <div className="opacity-layer"></div>
+            <ContentWrapper>
+                <div className="heroBannerContent">
+                    <span className="title">Welcome.</span>
+                    <span className="subTitle">
+                        Millions of movies, TV shows and people to discover.
+                        Explore now.
+                    </span>
+                    <div className="searchInput">
+                        <input
+                            type="text"
+                            placeholder="Search for a movie or tv show...."
+                            onChange={(e) => setQuery(e.target.value)}
+                            onKeyUp={searchQueryHandler}
+                        />
+                        <button>Search</button>
+                    </div>
+                </div>
+            </ContentWrapper>
         </div>
-      )}
-      <div className="opacity-layer">
-        
-      </div>
-
-      <ContentWrapper>
-        <div className="hero-banner-content">
-          <span className="title">Welcome</span>
-          <span className="sub-title">
-            Millions of movies,TV shows and people to discover . Explore Now{" "}
-          </span>
-          <div className="search-input">
-            <input
-              type="text"
-              placeholder="search for movies and TV shows..... "
-              onChange={(e) => setQuery(e.target.value)}
-              onKeyup={searchqueryhandler}
-            />
-            <button> Search </button>
-          </div>
-        </div>
-      </ContentWrapper>
-    </div>
-  );
+    );
 };
 
 export default HeroBanner;
