95874322562cdd4f969f6d765772708577f526df
Ishu Pathak <pishu1766@gmail.com>
2023-08-23 12:09:35 +0530

    basic structure of details page done

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import "./Details.scss"
-
+import React from "react";
+import "./Details.scss";
+import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
 const Details = () => {
-  return (
-    <div>Details</div>
-  )
-}
 
-export default Details
+
+// const { mediaType, id } = useParams();
+// const { data, loading } = useFetch(`/${mediaType}/${id}`);
+
+
+  return <div>
+    <DetailsBanner/>
+  </div>;
+};
+
+export default Details;
