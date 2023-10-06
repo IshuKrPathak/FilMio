4d9c2060fbb0f11e971335967183732b92143d9d
Ishu Pathak <pishu1766@gmail.com>
2023-10-06 15:19:57 +0530

    details banner   updated

--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -9,7 +9,7 @@ import ContentWrapper from "../../../components/contentWrapper/ContentWrapper.js
 import useFetch from "../../../hooks/useFetch";
 import Genres from "../../../components/Genres/Genres";
 import CircleRating from "../../../components/circleRating/CircleRating";
-import Img from "../../../components/lazyLoadImage/Img.jsx";
+import Img from "../../..//components/lazyLoadimage/Img";
 import PosterFallback from "../../../assets/no-poster.png";
 import { PlayIcon } from "../PlayButton";
 import VideoPopup from "../../../components/videoPopup/VideoPopup";
